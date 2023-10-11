0c165a4cd2474e2d239a3b79c5ec845c07c0b674
Dev-Chitara <141655519+Dev-Chitara@users.noreply.github.com>
2023-10-11 15:26:17 +0530

    Add files via upload

--- schemas/products.py
+++ schemas/products.py
@@ -1,25 +1,118 @@
 import uuid
+import csv
+
 
 class Product:
-    def __init__(self,name,description,price,quantity):
+
+    def __init__(self, name, description, price, quantity):
         self.id = uuid.uuid4()
         self.name = name
         self.description = description
-        self.price = price 
+        self.price = price
         self.quantity = quantity
 
-    def update_quantity(self,new_quantity):
-        if type(new_quantity)==int:
+    def set_product(self, new_name, new_description, new_price, new_quantity):
+        if type(self.name) == str:
+            self.name = new_name
+        else:
+            print("Invalid Name")
+
+        if type(self.description) == str:
+            self.description = new_description
+        else:
+            print("Invalid Description")
+
+        if type(self.price) == int:
+            self.price = new_price
+        else:
+            print("Invalid Amount")
+
+        if type(self.quantity) == int:
             self.quantity = new_quantity
         else:
             print("Invalid Quantity")
 
     def get_product_details(self):
-        return f"""Product Name is : {self.name}
-Product Price is : {self.price}
-and Quantity is : {self.quantity}"""
-    
-      
-def initializing_obj(name,description,price,quantity):
-    return Product(name,description,price,quantity)
+        return self.__dict__
 
+
+p1 = Product("Apple", "Apple is very delicious", 20, 4)
+p2 = Product("Mango", "Mango is very delicious", 25, 2)
+p3 = Product("Banana", "Banana is very delicious", 30, 4)
+p4 = Product("Grapes", "Grapes is very delicious", 40, 20)
+p5 = Product("Watermelon", "Watermelon is very delicious", 35, 1)
+p6 = Product("Strawberry", "Strawberry is very delicious", 30, 10)
+
+
+def get_products():
+    list1 = []
+    file = open("./db/products.csv", "r")
+    reader = csv.DictReader(file)
+
+    for item in reader:
+        list1.append(item["name"])
+
+    file.close()
+    return list1
+
+# print(get_products())
+
+
+instance = [p1, p2, p3, p4]
+
+# This fuction for store product dictionary in list
+
+
+def list_of_products(persons):
+    lst = []
+    for i in persons:
+        lst.append(i.get_product_details())
+    return lst
+
+
+l = list_of_products(instance)
+
+
+def create_products():
+    file = open("./db/products.csv", "w")
+
+    fields = ["id", "name", "description", "price", "quantity"]
+    writer = csv.DictWriter(file, fieldnames=fields)
+    writer.writeheader()
+    writer.writerows(l)
+    file.close()
+
+# create_products()
+
+
+def get_product(id):
+
+    file = open("./db/products.csv", "r")
+    reader = csv.DictReader(file)
+
+    for item in reader:
+        if item["id"] == id:
+            print(item)
+    file.close()
+
+# get_product("5c0e3e7d-c44d-4e07-8e41-152777468cb2")
+
+
+def update_product(id, name, description, price, quantity):
+    pass
+
+
+def delete_product(id):
+    pass
+
+
+# initialization of a product
+# set values of a product  - update object values
+# get values of a product - return dictionary
+
+
+# . get_products  - list of products
+# 2. create_products - initialize and save products
+# 3. get_product -  return single details of a product
+# 4. update_product - update and return single details of a product
+# 5. delete_product - delete a product
--- schemas/suppliers.py
+++ schemas/suppliers.py
@@ -1,16 +1,22 @@
 import uuid
+
+
 class Supplier:
+
     def __init__(self,name,contact_info):
         self.id = uuid.uuid4()
         self.name = name
         self.contact_info = contact_info
 
+
     def supplier_details(self):
         return f"""Supplier Name is : {self.name}
 and Supplier Contact Number is : {self.contact_info }"""
     
+
 def initializing_obj(name,contact_info):
         return Supplier(name,contact_info)
+
 
 s1=initializing_obj("Rahul","123456789")
 print(s1.supplier_details())
