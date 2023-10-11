7c279b36111ddcbb80e143bdaf387c071fbd35a0
Dev-Chitara <141655519+Dev-Chitara@users.noreply.github.com>
2023-10-11 19:46:27 +0530

    Add files via upload

--- schemas/products.py
+++ schemas/products.py
@@ -1,5 +1,6 @@
 import uuid
 import csv
+from functools import wraps
 
 
 class Product:
@@ -36,12 +37,12 @@ class Product:
         return self.__dict__
 
 
-p1 = Product("Apple", "Apple is very delicious", 20, 4)
-p2 = Product("Mango", "Mango is very delicious", 25, 2)
-p3 = Product("Banana", "Banana is very delicious", 30, 4)
-p4 = Product("Grapes", "Grapes is very delicious", 40, 20)
-p5 = Product("Watermelon", "Watermelon is very delicious", 35, 1)
-p6 = Product("Strawberry", "Strawberry is very delicious", 30, 10)
+# p1 = Product("Apple", "Apple is very delicious", 20, 4)
+# p2 = Product("Mango", "Mango is very delicious", 25, 2)
+# p3 = Product("Banana", "Banana is very delicious", 30, 4)
+# p4 = Product("Grapes", "Grapes is very delicious", 40, 20)
+# p5 = Product("Watermelon", "Watermelon is very delicious", 35, 1)
+# p6 = Product("Strawberry", "Strawberry is very delicious", 30, 10)
 
 
 def get_products():
@@ -55,35 +56,24 @@ def get_products():
     file.close()
     return list1
 
-# print(get_products())
 
 
-instance = [p1, p2, p3, p4]
-
-# This fuction for store product dictionary in list
-
-
-def list_of_products(persons):
-    lst = []
-    for i in persons:
-        lst.append(i.get_product_details())
-    return lst
-
-
-l = list_of_products(instance)
-
-
-def create_products():
-    file = open("./db/products.csv", "w")
+def create_products(product,name, description, price, quantity):
+    p=product(name, description, price, quantity)
+    d=p.get_product_details()
+    file = open("./db/products.csv", "a")
 
     fields = ["id", "name", "description", "price", "quantity"]
     writer = csv.DictWriter(file, fieldnames=fields)
-    writer.writeheader()
-    writer.writerows(l)
+    writer.writerow(d)
     file.close()
 
-# create_products()
-
+# create_products(Product,"Apple", "Apple is very delicious", 20, 4)
+# create_products(Product,"Mango", "Mango is very delicious", 25, 2)
+# create_products(Product,"Banana", "Banana is very delicious", 30, 4)
+# create_products(Product,"Grapes", "Grapes is very delicious", 40, 20)
+# create_products(Product,"Watermelon", "Watermelon is very delicious", 35, 1)
+# create_products(Product,"Strawberry", "Strawberry is very delicious", 30, 10)
 
 def get_product(id):
 
@@ -91,15 +81,44 @@ def get_product(id):
     reader = csv.DictReader(file)
 
     for item in reader:
-        if item["id"] == id:
+        if item["id"]==id:
             print(item)
     file.close()
+    
 
-# get_product("5c0e3e7d-c44d-4e07-8e41-152777468cb2")
+# get_product("6478da5e-82b7-44e4-825b-deac414639e3")
 
 
 def update_product(id, name, description, price, quantity):
-    pass
+    file = open("./db/products.csv", "r+")
+    reader = csv.DictReader(file)
+
+    L=[]
+    found=False
+
+    for row in reader:
+        if row["id"]==id:
+            found=True
+            row["name"]=name
+            row["description"]=description
+            row["price"]=price
+            row["quantity"]=quantity
+        L.append(row)
+    file.close()
+    if found==False:
+        print("Fruit not Found")
+    else:
+        file=open("./db/products.csv","w+",newline="")
+        fields = ["id", "name", "description", "price", "quantity"]
+        writer=csv.DictWriter(file,fieldnames=fields)
+        writer.writerows(L)
+        file.seek(0)
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(row)
+        file.close()
+update_product("95e0f7e8-4715-4dbf-be57-50c41eb94a70","pappya", "Apple is very delicious", 20, 4)
+
 
 
 def delete_product(id):
